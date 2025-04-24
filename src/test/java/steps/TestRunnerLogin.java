58d1bf80655761ccbd28741351c0fd888d83033c
mc.raganelli <mc.raganelli@siinfo.eu>
2025-04-24 16:46:39 +0200

    add report

--- src/test/java/steps/TestRunnerLogin.java
+++ src/test/java/steps/TestRunnerLogin.java
@@ -5,7 +5,8 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/features", glue={"steps"})
+@CucumberOptions(features="src/test/resources/features", glue={"steps"},
+        plugin = "json:target/cucumber-reports/CucumberTestReport.json")
 public class TestRunnerLogin {
 
 }
